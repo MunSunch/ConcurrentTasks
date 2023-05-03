24406fd37515b719c589877acc647e97c15e9f33
MunirTheBest <msunchalyaev@gmail.com>
2023-05-03 14:02:58 +0300

    Add task2

--- src/lesson1/Test.java
+++ src/lesson1/Test.java
@@ -3,19 +3,21 @@ package lesson1;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.concurrent.*;
 
 public class Test {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws Exception {
         int countWords = 25;
         String[] texts = new String[countWords];
         for (int i = 0; i < texts.length; i++) {
             texts[i] = generateText("aab", 30_000);
         }
 
-        List<Thread> threads = new ArrayList<>(countWords);
+        ExecutorService service = Executors.newFixedThreadPool(countWords);
+        List<Future<Integer>> futures = new ArrayList<>(countWords);
         long startTs = System.currentTimeMillis(); // start time
         for (String text : texts) {
-            Thread newThread = new Thread(() -> {
+            Callable<Integer> callable = () -> {
                 int maxSize = 0;
                 for (int i = 0; i < text.length(); i++) {
                     for (int j = 0; j < text.length(); j++) {
@@ -34,18 +36,22 @@ public class Test {
                         }
                     }
                 }
-                System.out.println(text.substring(0, 100) + " -> " + maxSize);
-            });
-            threads.add(newThread);
-            newThread.start();
+                return maxSize;
+            };
+            var future = service.submit(callable);
+            futures.add(future);
         }
         long endTs = System.currentTimeMillis(); // end time
 
-        for (Thread thread : threads) {
-            thread.join();
+        int maxCount = 0;
+        for(var future: futures) {
+            int currentCount = future.get();
+            maxCount = Math.max(maxCount, currentCount);
         }
+        service.shutdownNow();
 
         System.out.println("Time: " + (endTs - startTs) + "ms");
+        System.out.println("Max: " + maxCount);
     }
 
     public static String generateText(String letters, int length) {
